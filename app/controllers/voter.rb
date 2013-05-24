3d1cb74657a310d2c920c68c0e4b93c365a38488
Apprentice <apprentice@devbootcamp.com>
2013-05-24 15:27:39 -0700

    save a vote

--- app/controllers/voter.rb
+++ app/controllers/voter.rb
@@ -5,8 +5,17 @@ end
 
 get '/survey/:survey_id' do 
  @survey = Survey.find(params[:survey_id])
- erb :survey 
- 
+ erb :survey  
+end
+
+post '/survey/:survey_id' do
+ @choice = Choice.find(params[:choice_id])
+ @survey = Survey.find(params[:survey_id])
+ @voter_id = 1
+
+ @answer = Answer.create(voter_id: @voter_id, choice_id: @choice.id)
+ redirect '/surveys'
+
 end
 
 
