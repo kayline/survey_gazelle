3e98c444abb94829f6bd66d805d34e5b91481dce
Apprentice <apprentice@devbootcamp.com>
2013-05-24 15:31:53 -0700

    kill old migration

--- app/models/choices_user.rb
+++
@@ -1,5 +0,0 @@
-class ChoicesUser < ActiveRecord::Base
-  # Remember to create a migration!
-  belongs_to :voter, :class_name => "User"
-  belongs_to :choice
-end
--- db/migrate/20130524113147_create_choices_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateChoicesUsers < ActiveRecord::Migration
-  def change
-        create_table :choices_users do |t|
-      t.references :voter
-      t.references :choice
-      t.timestamps
-    end
-  end
-end
