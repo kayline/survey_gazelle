75a96bce995fa5c010e656e831809169c654a1c3
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-05-26 11:27:59 -0700

    minor form formatting

--- app/controllers/voter.rb
+++ app/controllers/voter.rb
@@ -10,18 +10,19 @@ get '/survey/:survey_id' do
 end
 
 post '/survey/:survey_id' do
-  p params
   @survey = Survey.find(params[:survey_id])
-  p @survey.id
-  @voter_id = current_user.id
-  @questions = Survey.find(@survey.id).questions
-  @questions.each do |q|
-    p q
-    p params[q.id.to_s.to_sym]
-    @chosen= get_choice(q.id, params[q.id.to_s.to_sym])
-    @answer = Answer.create(voter_id: @voter_id, choice_id: @chosen.id)
+  if current_user
+    @voter_id = current_user.id
+    @questions = Survey.find(@survey.id).questions
+    @questions.each do |q|
+      @chosen= get_choice(q.id, params[q.id.to_s.to_sym])
+      @answer = Answer.create(voter_id: @voter_id, choice_id: @chosen.id)
+    end
+    redirect '/surveys'
+  else
+    redirect "/survey/#{@survey.id}"
+    # we need error messages!
   end
-  redirect '/surveys'
 
 end
 
