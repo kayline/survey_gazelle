2e8d5a9b99079dd46d9b25692029caac694f5327
Apprentice <apprentice@devbootcamp.com>
2013-05-26 12:48:53 -0700

    fixed merge conflicts

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,21 @@
-var choice = "<input name='survey[choices][][choice]'' type='text' placeholder='Answer'>"
+var form = "<div class='question-box container'>"+
+              "<input name='survey[question]' type='text' placeholder='Question'>"+
+              "<input name='survey[choices][][choice]' type='text' placeholder='Answer'>"+
+              "<input name='survey[choices][][choice]' type='text' placeholder='Answer'>"+
+              "<button class='add_choice' type='button'>Add Choice</button>"+
+            "</div>";
+var choice = "<input name='survey[choices][][choice]' type='text' placeholder='Answer'>";
 
 $(document).ready(function() {
-  $('.add_choice').on('click', function(){
-    var submit_button = $('.submit').detach();
-    var survey = $('.survey')
-    $(choice).appendTo(survey);
-    $(submit_button).appendTo(survey);
+  $('.add_question').on('click', function(){
+    $(form).appendTo($('form').last());
+    $('.add_buttons').appendTo($('form').last());
+  });
+
+  $(document).on('click','.add_choice' ,function(){
+    $(choice).appendTo($(this).parent());
+    $(this).appendTo($(this).parent());
   });
 });
+
+
